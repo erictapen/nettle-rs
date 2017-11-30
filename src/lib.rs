8052d964565f754b707e11096f7634784f5bfb26
Kai Michaelis <kai.michaelis@pep-project.org>
2017-11-30 17:14:39 +0100

    fix sha1 export

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 extern crate nettle_sys;
 
+mod hash;
+pub use hash::Hash;
+
 mod sha1;
-pub use sha1::{
-    Sha1,
-    SHA1_DIGEST_SIZE,
-};
+pub use sha1::Sha1;
+
+mod sha256;
+pub use sha256::Sha256;
