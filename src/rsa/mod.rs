36e045ae4673cc02cb059e0ac071ce03d6832be2
Neal H. Walfield <neal@pep.foundation>
2018-04-23 11:45:45 +0200

    Export verify_digest_pkcs1 and the associated ASN.1 constants.

--- src/rsa/mod.rs
+++ src/rsa/mod.rs
@@ -10,8 +10,16 @@ pub use self::keys::{
 mod pkcs1;
 pub use self::pkcs1::{
     Pkcs1Hash,
+    ASN1_OID_MD2,
+    ASN1_OID_MD5,
+    ASN1_OID_SHA1,
+    ASN1_OID_SHA224,
+    ASN1_OID_SHA256,
+    ASN1_OID_SHA384,
+    ASN1_OID_SHA512,
     sign_pkcs1,
     verify_pkcs1,
+    verify_digest_pkcs1,
     encrypt_pkcs1,
     decrypt_pkcs1,
 };
